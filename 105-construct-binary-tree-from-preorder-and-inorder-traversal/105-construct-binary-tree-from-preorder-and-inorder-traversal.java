85456628da6aadc8b960a99b147cee5216e68e6c
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-02 20:41:01 +0530

    Time: 3 ms (81.41%), Space: 44.5 MB (37.84%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,11 +17,15 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
         int[] rootindex = new int[1];
-        return constructtree(preorder , inorder , 0 , preorder.length , rootindex );
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int j = 0 ; j < preorder.length ;j++){
+            map.put(inorder[j] , j);
+        }
+        return constructtree(preorder , inorder , 0 , preorder.length , rootindex, map );
         
     }
     
-    public static TreeNode constructtree(int[] pre , int[] in , int start , int end , int[] rootindex){
+    public static TreeNode constructtree(int[] pre , int[] in , int start , int end , int[] rootindex, HashMap map){
         
         if(start> end || rootindex[0] >= in.length){
             
@@ -35,18 +39,20 @@ class Solution {
         
         int i = 0;
         
-        for(i = start ; i < end ;i++ ){
+//         for(i = start ; i < end ;i++ ){
             
-            if(in[i]==pre[rootindex[0]]){
-                break;
-            }
+//             if(in[i]==pre[rootindex[0]]){
+//                 break;
+//             }
             
-        }
+//         }
+        
+       i =(int)map.get(pre[rootindex[0]]);
         
         rootindex[0]++;
         
-        result.left = constructtree(pre,in,start,i-1 ,rootindex );
-        result.right= constructtree(pre,in,i+1,end ,rootindex );
+        result.left = constructtree(pre,in,start,i-1 ,rootindex, map);
+        result.right= constructtree(pre,in,i+1,end ,rootindex, map );
         
         return result;
     }
