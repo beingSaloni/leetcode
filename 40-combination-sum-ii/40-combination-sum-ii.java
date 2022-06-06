513b5dd2c7c6d76e5b201301e5f69262e97d29fc
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-06 23:02:49 +0530

    Time: 10 ms (20.18%), Space: 44 MB (45.15%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+       List<List<Integer>> outer = new ArrayList<List<Integer>>();
+        
+        List<Integer> inner = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        
+        com(candidates , 0 , target , outer , inner);
+        
+        return outer;
+        
+        
+    }
+    
+    public static void com(int[] can , int i , int t , List<List<Integer>> outer  ,   List<Integer> inner ){
+        
+        if(t==0){
+            
+            if(!outer.contains(inner)){
+                
+                outer.add(new ArrayList<>(inner));
+            }
+            
+            
+            return;
+        }
+        
+        if(t<0 || i>= can.length){
+
+            
+            return;
+        }
+        
+        for(int j = i ; j < can.length ;j++){
+            if(j > i && can[j]==can[j-1]){
+                continue;
+            }
+             inner.add(can[j]);
+          com(can , j+1 , t- can[j] , outer, inner);
+           inner.remove(inner.size()-1);
+            
+        }
+        
+//         com(can , i+1 , t , outer , inner);
+//         inner.add(can[i]);
+//         com(can , i+1 , t- can[i] , outer, inner);
+//         inner.remove(inner.size()-1);
+        
+        
+    }
+    
+    
+}
