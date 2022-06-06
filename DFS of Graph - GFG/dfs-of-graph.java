35d544d47b585470ac68583f70e6600472e10596
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-06 18:15:39 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        HashSet<Integer> visited = new HashSet<>();
+        
+        dfs(V , adj , ans , visited , 0);
+        
+        return ans;
+        
+    }
+    
+    public static void dfs(int V, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ans,HashSet<Integer> visited , int currV){
+        
+        if(visited.contains(currV)){
+            return ;
+        }
+        
+        visited.add(currV);
+        ans.add(currV);
+        
+        List<Integer> neighours = adj.get(currV);
+        
+        for(int i = 0 ; i<neighours.size() ;i++){
+            
+            dfs(V, adj , ans , visited, neighours.get(i));
+        }
+        
+        
+    }
+    
+}
