f6bae701300e24eac641bc1590972e9c05cdf453
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-11 16:12:05 +0530

    Time: 242 ms (5.09%), Space: 204.7 MB (5.09%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,22 +1,39 @@
 class Solution {
-    public int minOperations(int[] A, int x) {
-    int n = A.length, res = n + 1;
-    Map<Integer, Integer> presum = new HashMap<>();
-    presum.put(0, -1);
-
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += A[i];
-        presum.put(sum, i);
-    }
-
-    sum = 0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (presum.containsKey(x - sum)) {
-            res = Math.min(res, n - i + presum.get(x - sum));
+    public int minOperations(int[] nums, int x) {
+        
+        int result = nums.length +1;
+        
+        int sum = 0 ;
+        
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        
+        memo.put(0,-1);
+       
+        for(int i  = 0 ; i < nums.length ; i++){
+            
+            sum+=nums[i];
+            
+            memo.put(sum,i);
+            
+            
         }
-        sum += A[i];
+        
+        sum =  0;
+        
+        for(int i = nums.length-1 ; i>= 0 ; i--){
+            
+            if(memo.containsKey(x-sum)){
+                
+                result = Math.min(result,nums.length-i+memo.get(x-sum)) ;
+            }
+            
+            sum+=nums[i];
+            
+        }
+        
+       if(result == nums.length+1){
+           return -1;
+       }
+        return result;
     }
-    return res == n + 1 ? -1 : res;
-}
 }
