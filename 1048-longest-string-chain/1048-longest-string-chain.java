242e466da88024e3e7f74b9f777ad00b9ea14afa
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-15 14:43:03 +0530

    Time: 88 ms (23.01%), Space: 67.1 MB (30.60%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -7,7 +7,12 @@ class Solution {
 	
         int ans = 0;
 	
+        // Sorting on the basis of length of words
         Arrays.sort(words, (a,b)-> a.length()-b.length());
+        
+        //  ["a","b","ba","bca","bda","bdca"]
+        
+        
 	    HashMap<String, Integer> map = new HashMap(); 
         
         
@@ -17,13 +22,23 @@ class Solution {
             
             map.put(st,1);
             
+            //  bdca 1 
+            
             for(int i = 0 ; i < st.length() ; i++){
                 
                 StringBuilder sb = new StringBuilder(st);
+                // bdca
                 
                String w = sb.deleteCharAt(i).toString();
+                // dca
+                // bca
+                // bda
+                // bdc
                 
                 if(map.containsKey(w) && map.get(w)+1 > map.get(st)){
+                    
+                    // st , 4--> bca (bca --> b , ba, bca)
+                
                     
                     map.put(st, map.get(w)+1);     
                 }
