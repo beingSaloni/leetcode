2897dfec111617b962864cc0a67e31410f2d62c2
Saloni Saxena <salonisaxena7777@gmail.com>
2022-06-30 21:49:55 +0530

    Time: 4 ms (90.02%), Space: 45.4 MB (68.68%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,16 @@
+
+        
+       public class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
+        int count = 0;
+        while(i < j){
+            count += nums[j]-nums[i];
+            i++;
+            j--;
+        }
+        return count;
+    }
+}
+    
