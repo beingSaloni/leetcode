62fa0155aaecd61a67c73fd8f2d7ddc59035f614
Saloni Saxena <salonisaxena7777@gmail.com>
2022-07-14 14:02:02 +0530

    Time: 12 ms (7.48%), Space: 42.7 MB (30.68%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -2,12 +2,12 @@ class Solution {
     public int numDecodings(String s) {
         
         int[] arr = new int[s.length()];
-        
+        //converting string to array
         for(int i = 0 ; i < s.length() ; i++){
             
             arr[i] = s.charAt(i)- '0';
             
-            // System.out.println(arr[i]);
+         
         }
         int[] memo = new int[s.length()];
     
@@ -19,45 +19,36 @@ class Solution {
         
         
         if(arr.length <= start){
-          // System.out.println(s);
+          
         
             return 1 ;
         }
         
-//         if(start==0 && arr[start] == 0 ){
-//             return 0;
-//         }
-//         if(start -1 >= 0 && arr[start] == 0  && arr[start-1] >= 3){
-            
-            
-//             return 0;
-//         }
         
         if(memo[start] != 0){
             
            return  memo[start];
         }
         
-    int cz = 0;
+    int considerOneDigit = 0;
         
-        if(arr[start] > 0){
-            cz = solve(arr , start + 1 ,  memo);
+        if(arr[start] > 0){   // to check no leading zero is considered
+             considerOneDigit= solve(arr , start + 1 ,  memo);
         }else{
             
             return 0;
         }
         
-        int cc = 0;
-        if(start+1 < arr.length && (arr[start]*10 + arr[start+1]) <= 26 ){
+        int considerTwoDigit = 0;
+        if(start+1 < arr.length && (arr[start]*10 + arr[start+1]) <= 26 ){ 
+                                        // We can consider two digits only if it is smaller than 26 
             
-            cc+= solve(arr , start + 2 ,  memo);
+            considerTwoDigit+= solve(arr , start + 2 ,  memo);
         }
         
-         // System.out.println(arr[i]);
+        memo[start] = considerOneDigit + considerTwoDigit ;
         
-        memo[start] = cz + cc ;
-        
-        return cz +cc;
+        return considerOneDigit +considerTwoDigit;
         
       
         
