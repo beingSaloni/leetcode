41635faf20c819af9301e2638546a90e08ee700a
Saloni Saxena <salonisaxena7777@gmail.com>
2022-07-17 06:41:42 +0530

    Time: 39 ms (53.01%), Space: 54.4 MB (18.07%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -0,0 +1,35 @@
+class Solution {
+   
+        
+        // 1 2 3
+        // 2 1 3
+        // 3 1 2
+        // 1 3 2
+        // 2 3 1
+        // 3 2 1
+        
+        // remove all the permutations 
+        // count how many inverse are there in each of it
+        // if k==cout; ans++
+        
+        // redo it 
+        public static int kInversePairs(int n, int k) {
+        int mod = 1000000007;
+        if (k > n*(n-1)/2 || k < 0) return 0;
+        if (k == 0 || k == n*(n-1)/2) return 1;
+        long[][] dp = new long[n+1][k+1];
+        dp[2][0] = 1;
+        dp[2][1] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= Math.min(k, i*(i-1)/2); j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                if (j >= i) dp[i][j] -= dp[i-1][j-i];
+                dp[i][j] = (dp[i][j]+mod) % mod;
+            }
+        }
+        return (int) dp[n][k];
+    }
+        
+    
+}
