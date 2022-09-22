205c6f62c2521684fc2b4583f9f67aa58a0c4ece
Saloni Saxena <salonisaxena7777@gmail.com>
2022-09-22 17:25:30 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (74.13%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -0,0 +1,93 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+        return solve(head,k);
+        
+       
+        
+    }
+    
+    private ListNode solve(ListNode head, int k){
+        
+        if(head == null){
+            return null;
+        }
+        
+        ListNode ans = valid(head,k);
+        if(ans != null){
+            
+            ListNode temp = ans.next;
+            
+            
+            ans.next =null ;
+            
+            ListNode reversedLL = reverseList(head);
+            head.next = solve(temp,k);
+            
+            return reversedLL ;
+            
+            
+            
+        }
+        
+        return head;
+        
+        
+    }
+    
+      public ListNode reverseList(ListNode head) {
+        
+        
+        ListNode prev = null ;
+        
+        ListNode tempNode = null ;
+        
+        ListNode current = head ;
+        
+        
+        
+        while(current != null ){
+            
+           tempNode = current.next ;
+            current.next = prev ;
+            prev = current ;
+            current = tempNode ;
+            
+        }
+        
+       
+        
+        return prev;
+        
+        
+        
+        
+    }
+    
+    private ListNode valid(ListNode head, int k){
+        
+        int r = 1;
+        ListNode temp = head ;
+        
+        while(temp!= null && r < k){
+            
+            temp = temp.next ;
+            r++;
+            
+        }
+        
+        return temp ;
+        
+    }
+    
+}
