bd8fb5fd14cd0c8938161c63cb5464eb60afd393
Saloni Saxena <salonisaxena7777@gmail.com>
2023-08-10 19:57:50 +0530

    Time: 8 ms (11.18%), Space: 43.4 MB (93.37%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,49 +1,31 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
-       ArrayList<Integer> inner = new ArrayList<>(); 
-        
-         List<List<Integer>> outer = new ArrayList<List<Integer>>(); 
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> inner = new ArrayList<>();
         Arrays.sort(nums);
-        helper(nums , 0 , inner , outer );
-        
-
-        
-        return outer ;
+        solve(ans , nums, 0 , inner);
+        return ans;
     }
     
-    public static void helper(int[] nums , int i , ArrayList inner , List<List<Integer>> outer){
+    private void solve(List<List<Integer>> ans , int[] nums, int index , List<Integer> inner ){
         
-        
-        if(nums.length <= i){
+        if(index >= nums.length){
+           
+            List<Integer> result = new ArrayList<>(inner);
             
-            List<Integer> rr = new ArrayList<Integer>(inner); // creating new list rr which is independent                                                           // from getting affected to changes in inner  
-            
-            
-     // outer.add(inner); ----> this won't work bcoz inner would change with recursive calls
-         if(!outer.contains(rr)){
-            outer.add(rr);                   // we are passing same outer every time 
-         }
-            return  ;
+            if(!ans.contains(result)){
+            ans.add(result);
+            }
+            return ;
         }
         
+     
+        inner.add(nums[index]);
+        solve(ans, nums , index +1, inner);
         
-        
-             
- 
-        
-        inner.add(nums[i]);
-        
-        helper(nums, i+1 , inner, outer);
         inner.remove(inner.size()-1);
-        
-             helper(nums, i+1 , inner , outer);
-        
-        
-       
-    }
-    
-    
+        solve(ans, nums, index + 1, inner);
+    } 
     
 }
