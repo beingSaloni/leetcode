77056878c31d27ec2c8647e89fa3002e34b1b2ff
Saloni Saxena <salonisaxena7777@gmail.com>
2023-08-13 03:09:39 +0530

    Time: 353 ms (5.01%), Space: 139.6 MB (5.00%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,33 +1,40 @@
 class Solution {
     public int change(int amount, int[] coins) {
-   HashMap<String,Integer> memo = new HashMap<>();
-        int zz = coin(coins , amount  , 0, memo);
-        // if(zz== 10000){
-        //     return -1;
-        // }
-        return zz;
+        
+        HashMap<String , Integer> memo = new HashMap<>();
+        
+     return    solve(coins ,0 , amount , memo);
     }
     
-    public static int coin(int[] coins, int a , int s , HashMap memo){
+    private int solve(int[] coins , int index , int sum , HashMap memo){
         
-        if(a==0){
-            return 1;
+        String val = index + " " + sum ;
+            
+        if(memo.containsKey(val)){
+            
+            return (int)memo.get(val);
         }
-        if(s==coins.length){
+        
+        if(index >= coins.length){
+            
             return 0;
         }
-        String st = a + " " + s;
-        if(memo.containsKey(st)){
-            return (int)memo.get(st);
-        }
-        int c = 0;
-        if(coins[s]<=a){
-         c =coin(coins,a-coins[s],s,memo);
-        }
-        int nc = coin(coins, a , s+1,memo);
-        int zzz = nc + c;
-        memo.put(st,zzz);
         
-        return c + nc;
+        if(sum == 0){
+            
+            return 1;
+        }
+        if(sum < 0){
+            
+            return 0;
+        }
+        
+        int consider = solve(coins, index , sum - coins[index] , memo );
+        int notconsider = solve(coins,index + 1 , sum , memo);
+        
+        memo.put(val, consider + notconsider);
+        return consider + notconsider ;
+        
+        
     }
 }
