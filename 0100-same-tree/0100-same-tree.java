16d38f3bbea800daa49d3e0e685d0a8d06e45b58
Saloni Saxena <salonisaxena7777@gmail.com>
2024-02-27 03:17:12 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (13.23%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,75 +16,32 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if(q == null && p == null){
+       return checkIfSame(p,q);
+        
+    }
+    
+    private boolean checkIfSame(TreeNode p , TreeNode q){
+        
+        if(p == null && q == null){
             
             return true;
         }
         
-          if(q == null || p == null){
+        if(p == null || q== null){
             
             return false;
         }
         
-       Queue<TreeNode> q1 = new LinkedList<>();
-        
-        Queue<TreeNode> q2 = new LinkedList<>();
-        
-        q1.add(p);
-        q2.add(q);
-        
-        while(q1.size() > 0){
+        if(p.val!= q.val){
             
-            int size = q1.size();
-            
-            while(size-- > 0){
-                
-                TreeNode temp1 = q1.poll();
-                
-                TreeNode temp2 = q2.poll();
-                
-                if(temp1.val != temp2.val){
-
-                    return false;
-                }
-                if(temp1.left != null || temp2.left != null){
-                    
-                    if(temp2.left == null || temp1.left == null ){
-                      
-                        return false;
-                    }
-                    
-                    q1.add(temp1.left);
-                    q2.add(temp2.left);
-                    
-                }
-                
-                if(temp1.right != null || temp2.right != null){
-                    
-                    if(temp2.right == null || temp1.right == null ){
-                        
-                         
-                        return false;
-                    }
-                    
-                    q1.add(temp1.right);
-                    q2.add(temp2.right);
-                    
-                }
-                
-               
-                
-                
-            }
-            
-             if(q1.size() != q2.size()){
-                    return false;
-                }
-            
+            return false;
         }
         
-        return true;
+       boolean left = checkIfSame(p.left , q.left);
         
+        boolean right = checkIfSame(p.right , q.right);
+        
+        return left && right ;
         
     }
 }
