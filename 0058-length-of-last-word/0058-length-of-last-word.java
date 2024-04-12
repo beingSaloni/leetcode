317844f5588115776bc012b89a9144c1d60620f3
Saloni Saxena <salonisaxena7777@gmail.com>
2024-04-12 10:00:40 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (73.06%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int ans = 0 ;
+        
+        int i = s.length()-1;
+        while(i >= 0 && s.charAt(i) == ' ' ){
+            
+            i--;
+        }
+        
+        while( i >= 0 && s.charAt(i) != ' '){
+            ans++;
+            i--;
+        }
+        
+        return ans;
+    }
+}
