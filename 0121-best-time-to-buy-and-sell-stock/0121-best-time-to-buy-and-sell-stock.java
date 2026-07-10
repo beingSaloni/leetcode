87f6c7294fdabd08f50cb54940656a51a2738371
Saloni Saxena <salonisaxena7777@gmail.com>
2026-07-10 12:02:54 +0530

    Time: 1 ms (99.94%), Space: 94.4 MB (51.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
+//         Kadane's Algorithm
+// Kadane's Algorithm is a dynamic programming technique used to find the maximum subarray sum in an array of numbers. The algorithm maintains two variables: max_current represents the maximum sum ending at the current position, and max_global represents the maximum subarray sum encountered so far. At each iteration, it updates max_current to include the current element or start a new subarray if the current element is larger than the accumulated sum. The max_global is updated if max_current surpasses its value.
+
         int buy = prices[0];
         int profit = 0 ;
 
