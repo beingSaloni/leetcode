ab87be0d737a59ceba643a6b7058047293d7afdd
Saloni Saxena <salonisaxena7777@gmail.com>
2026-07-19 17:32:10 +0530

    Time: 7 ms (12.06%), Space: 46.2 MB (17.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        String s = "" + x ;
+
+        for(int i = 0 ; i < s.length()/2 ; i++){
+
+            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+
+                return false;
+            }
+
+
+        }
+
+        return true;
+        
+    }
+}
